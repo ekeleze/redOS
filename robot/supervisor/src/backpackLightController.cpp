919abb4dce77dcba4f46962c39da51d14517f405
froggitti <85637569+froggitti@users.noreply.github.com>
2025-04-19 17:58:22 -0400

    make post-power on lights RED!

--- robot/supervisor/src/backpackLightController.cpp
+++ robot/supervisor/src/backpackLightController.cpp
@@ -111,13 +111,13 @@ namespace BackpackLightController {
     {
       u32 color;
       if(i == 0) {
-        color = 0x80ff0000; // red for back led
+        color = 0xFF0000; // red for back led
       } else if(i == 1) {
-        color = 0x8000ff00; // green for middle led
+        color = 0xFF0000; // green for middle led
       } else if(i == 2) {
-        color = 0x800000ff; // blue for top led
+        color = 0xFF0000; // blue for top led
       } else {
-        color = 0x80808000; // fallback color
+        color = 0xFF0000; // fallback color
       }
     
       _ledParams[(int)BackpackLightLayer::BPL_USER].lights[i] = {
